2183362e443030ad4a20b22a2a77fc511ed178a9
Jimmy Hough Jr <jimmyhoughjr@Jimmys-iMac.local>
2015-09-19 22:05:41 -0500

    added route for new

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
  get 'temperatures' => 'temperatures#index'
+ get 'temperatures/new' => 'temperatures#new'
  
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
