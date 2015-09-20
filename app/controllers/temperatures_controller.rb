3cb1abce90c0f51516c767ab59c99eb1bbc34c17
Jimmy Hough Jr <jimmyhoughjr@Jimmys-iMac.local>
2015-09-19 22:06:18 -0500

    added temperatures controller

--- app/controllers/temperatures_controller.rb
+++ app/controllers/temperatures_controller.rb
@@ -1,6 +1,22 @@
 class TemperaturesController < ApplicationController
 
   def index
-    
+    @temperatures = Temperature.all
+    render json: @temperatures
+  end
+
+  def new
+    # render plain: params
+    @temperature = Temperature.new
+    @temperature.sensorID = params[:sensorID]
+    @temperature.value = params[:value]
+    @temperature.measuredAt = params[:measuredAt]
+
+    if @temperature.save
+      render plain: "200"
+    else
+      render plain: "temperature cannot be saved with params #{params}"\
+                    "because #{@temperature.errors.messages} "
+    end
   end
 end
