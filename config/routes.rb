b0bc9969cb43ded02992c58408ce253ca1b52501
Jimmy Hough Jr <jimmyhoughjr@Jimmys-iMac.local>
2015-10-04 15:25:58 -0500

    Added more scaffolds

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :sensor_errors
+  resources :smoke_levels
+  resources :ambient_light_levels
+  resources :heat_indices
+  resources :humidities
   resources :temperatures
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
