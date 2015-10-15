d14be420ad023b545a7f32cf1b7e21206a4beb60
Jimmy Hough Jr <jimmyhoughjr@Jimmys-iMac.local>
2015-10-14 21:10:54 -0500

    made timestamp helper more general

--- app/helpers/timestamp_params_helper.rb
+++ app/helpers/timestamp_params_helper.rb
@@ -1,17 +1,17 @@
 module TimestampParamsHelper
-  def scrubbedParams(timestamp_params)
-    year = humidity_params["measuredAt(1i)"].to_i
-    month = humidity_params["measuredAt(2i)"].to_i
-    day = humidity_params["measuredAt(3i)"].to_i
-    hour = humidity_params["measuredAt(4i)"].to_i
-    minute = humidity_params["measuredAt(5i)"].to_i
+
+  def packTimestampParams(timestamp_params)
+    year = timestamp_params["measuredAt(1i)"].to_i
+    month = timestamp_params["measuredAt(2i)"].to_i
+    day = timestamp_params["measuredAt(3i)"].to_i
+    hour = timestamp_params["measuredAt(4i)"].to_i
+    minute = timestamp_params["measuredAt(5i)"].to_i
 
     dateTimeFromParams = DateTime.new(year, month, day, hour, minute)
     excludedParams = ["measuredAt(1i)","measuredAt(2i)","measuredAt(3i)","measuredAt(4i)","measuredAt(5i)"]
 
-    theParams = humidity_params.except(*excludedParams)
+    theParams = timestamp_params.except(*excludedParams)
     theParams[:measuredAt] = dateTimeFromParams
-    # byebug
     return theParams
   end
 end
